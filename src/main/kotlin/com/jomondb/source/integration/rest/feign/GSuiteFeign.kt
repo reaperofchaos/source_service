a76cd5996225ac8a4dfe6f978012a31f32dfb92e
Jacob Conner <tokugawa111@gmail.com>
2025-08-13 20:27:10 -0400

    add some feigns

--- src/main/kotlin/com/jomondb/source/integration/rest/feign/GSuiteFeign.kt
+++ src/main/kotlin/com/jomondb/source/integration/rest/feign/GSuiteFeign.kt
@@ -2,11 +2,40 @@ package com.jomondb.source.integration.rest.feign
 
 import com.jomondb.source.integration.rest.dataobject.DownloadFile
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.multipart.MultipartFile
 
 @FeignClient(value = "gsuite", url="\${gateway.hostname}/" + "gsuite/")
 public interface GSuiteFeign {
-    @GetMapping("/folders", produces = ["application/json"])
+    @PostMapping("/file/upload", produces = ["application/json"])
+    fun uploadFile(@RequestParam("file") file: MultipartFile,
+                   @RequestParam("folderId") folderId: String?
+    ): ResponseEntity<String>
+
+    @GetMapping("/file/{id}")
+    fun download(@PathVariable id: String): ResponseEntity<ByteArray>
+
+    @GetMapping("/file/info/{name}")
+    fun getInfoByName(@PathVariable name: String): List<DownloadFile>
+
+    @GetMapping("/folder/info")
+    fun getFolderInfo(
+        @RequestParam("folderName") folderName: String,
+        @RequestParam("parentId") parentId: String
+    ): List<DownloadFile>
+
+    @GetMapping("/folder/folders", produces = ["application/json"])
     fun getFolders(): List<DownloadFile>
 
+    @PostMapping("/folder/create")
+    fun createFolder(
+        @RequestParam("folderName") folderName: String,
+        @RequestParam("parentId") parentId: String): ResponseEntity<String>
+
+    @GetMapping("/folder/files/{folderId}")
+    fun getFilesInFolder(@PathVariable folderId: String): List<DownloadFile>
 }
--- src/main/kotlin/com/jomondb/source/service/GSuiteService.kt
+++ src/main/kotlin/com/jomondb/source/service/GSuiteService.kt
@@ -2,16 +2,42 @@ package com.jomondb.source.service
 
 import com.jomondb.source.integration.rest.dataobject.DownloadFile
 import com.jomondb.source.integration.rest.feign.GSuiteFeign
-import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 
 @Service
 class GSuiteService {
     @Autowired
     private lateinit var gSuiteFeign: GSuiteFeign;
 
+    fun uploadFile(file: MultipartFile, folderId: String?): String{
+        val response = gSuiteFeign.uploadFile(file, folderId)
+        return response.body ?: throw RuntimeException("Upload failed - no response body")
+    }
+
+    fun downloadFile(id: String): ByteArray{
+        val response = gSuiteFeign.download(id);
+        return response.body ?: throw RuntimeException("Download failed - no response body")
+    }
+
+    fun getFileInfo(name: String): List<DownloadFile>{
+        return gSuiteFeign.getInfoByName(name);
+    }
+
+    fun getFolderInfo(folderName: String, parentId: String): List<DownloadFile>{
+        return gSuiteFeign.getFolderInfo(folderName, parentId);
+    }
+
     fun getFolders(): List<DownloadFile>{
         return gSuiteFeign.getFolders();
+    }
+
+    fun createFolder(folderName: String, parentId: String): String{
+        return gSuiteFeign.createFolder(folderName, parentId).body ?: throw RuntimeException("Create folder failed - no response body")
+    }
+
+    fun getFilesInFolder(folderId: String):List<DownloadFile>{
+        return gSuiteFeign.getFilesInFolder(folderId)
     }
 }
